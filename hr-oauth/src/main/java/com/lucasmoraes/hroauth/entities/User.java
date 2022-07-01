adea69676a2b6838678d23d110b9f07af08b1843
Lucas Moraes <lucasmoraes2@outlook.com.br>
2022-07-01 00:05:49 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/lucasmoraes/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/lucasmoraes/hroauth/entities/User.java
@@ -1,10 +1,16 @@
 package com.lucasmoraes.hroauth.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User {
+public class User implements UserDetails {
 
     private Long id;
     private String name;
@@ -70,5 +76,36 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
