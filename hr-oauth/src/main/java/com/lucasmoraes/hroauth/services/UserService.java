59c838a0966921878a223fbf32334a3f5798109f
Lucas Moraes <lucasmoraes2@outlook.com.br>
2022-06-30 22:57:21 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/lucasmoraes/hroauth/services/UserService.java
@@ -0,0 +1,27 @@
+package com.lucasmoraes.hroauth.services;
+
+import com.lucasmoraes.hroauth.entities.User;
+import com.lucasmoraes.hroauth.feignclients.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserFeignClient.class);
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    public User findByEmail(String email) {
+        User user = userFeignClient.findByEmail(email).getBody();
+        if (user == null) {
+            logger.error("Email not found: " + email);
+            throw new IllegalArgumentException("Email not found");
+        }
+        logger.info("Email found: " + email);
+        return user;
+    }
+}
